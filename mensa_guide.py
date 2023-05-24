839883390ea11e47364d95f9f7ea3ddcd50e51f1
tzschmidt <tzschmidt@gmx.de>
2023-05-24 10:38:04 +0200

    Add allergen handling; Add meal parser and selection; Refactoring

--- mensa_guide.py
+++ mensa_guide.py
@@ -2,38 +2,43 @@ import os
 import json
 import requests
 import datetime
+import re
 
 #%%
-def startup():
+# main program
+def main():
     # TODO variable data path
+    todayMeals = get_today_meals()
     student = input("Enter student-id: ")
     dataDir = "data"
     dataPath = "data/" + student + ".json"
-    ingredientsPath = "data/ingredients.json"
+    allergensPath = "data/allergens.json"
     if not os.path.isdir(dataDir):
         os.makedirs(dataDir)
         print("data folder created")
-    if not os.path.isfile(ingredientsPath):
-        open(ingredientsPath, 'a').close()
-        print("ingredients file created")
-        ingredients = []
-        save_data(ingredients, "ingredients")
+    if not os.path.isfile(allergensPath):
+        open(allergensPath, 'a').close()
+        print("allergens file created")
+        allergens = get_allergens(todayMeals)
+        save_data(allergens, "allergens")
     else:
-        print("ingredients file found")
-        ingredients = load_data("ingredients")
+        print("allergens file found")
+        allergens = load_data("allergens")
+        allergens = update_allergens(allergens, todayMeals)
     if not os.path.isfile(dataPath):
         open(dataPath, 'a').close()
         print("data file created")
-        profile = init_taste(student, ingredients)
+        profile = init_taste(student, allergens)
     else:
         print("data file found")
         profile = load_data(student)
-        profile = update_allergy(profile, ingredients)
-    return profile, ingredients
+        profile = update_allergy(profile, allergens)
+    return None
 
 #%%
+# initalize profile of new user
 def init_taste(student, ingredients):
-    profile = {"id": student, "diet":{}, "allergy":{}, "taste":{}}
+    profile = {"id": student, "diet":{}, "allergens":{}, "taste":{}}
     print("Diet:")
     for i in ["Vegetarisch", "Vegan"]:
         if input(i + "? ").lower() in ["yes", "y"]:
@@ -43,27 +48,46 @@ def init_taste(student, ingredients):
     print("Allergien:")
     for i in ingredients:
         if input(i + "? ").lower() in ["yes", "y"]:
-            profile["allergy"][i] = True
+            profile["allergens"][i] = True
         else:
-            profile["allergy"] = False
+            profile["allergens"][i] = False
     save_data(profile, student)
     return profile
 
 #%%
-def update_allergy(profile, ingredients):
-    print("New ingredients?")
-    for i in ingredients:
-        if i not in profile["allergy"]:
+# update allergen preferences of user
+def update_allergy(profile, allergens):
+    print("New allergens?")
+    for i in allergens:
+        if i not in profile["allergens"]:
             if input(i + "? ").lower() in ["yes", "y"]:
-                profile["allergy"][i] = True
+                profile["allergens"][i] = True
             else:
-                profile["allergy"][i] = False
+                profile["allergens"][i] = False
     print("Done")
     save_data(profile, profile["id"])
     return profile
     
-    
+# update allergens
+def update_allergens(allergens, meals):
+    new = get_allergens(meals)
+    for i in new:
+        if i not in allergens:
+            allergens.append(i)
+    print("allergens updated")
+    return allergens
+
+# get allergens from list of meals
+def get_allergens(meals):
+    allergens = []
+    for i in meals:
+        for j in i["allergens"]:
+            if j["type"] == "Allergen":
+                allergens.append(j["longName"])
+    return allergens
+
 #%%
+# load data from json
 def load_data(student):
     path = "data/" + student + ".json"
     with open(path) as infile:
@@ -72,6 +96,7 @@ def load_data(student):
     print("read")
     return data
 
+# save data to json
 def save_data(data, student):
     path = "data/" + student + ".json"
     json_data = json.dumps(data, indent=4)
@@ -81,6 +106,7 @@ def save_data(data, student):
     print("written")
 
 #%%
+# use api to get meals
 def get_all_meals():
     headers = {
         'accept': 'application/json;charset=utf-8',
@@ -94,7 +120,8 @@ def get_all_meals():
     response = requests.get('https://apiup.uni-potsdam.de/endpoints/mensaAPI/2.0/meals', params=params, headers=headers)
     return response.json()["meal"]
 
-
+# filter not needed meals
+# (not today, Nudeltheke, Abendangebot)
 def get_today_meals():
     meals = get_all_meals()
     day = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -106,17 +133,47 @@ def get_today_meals():
     return tmeals
 
 #%%
+# TODO integrate into main
+
+# parse name of meal into parts
+# WIP only simplified for now
+# no difference between "mit" and "ohne"
+# theorretically "ohne" should appear very rarely
+# no gramatical cases -> Pilzen != Pilze
+def parse_name(meal):
+    parts = []
+    words = re.split(",|mit|und|ohne|auf|bei|in|über|unter|zu",meal["description"])
+    for i in words:
+        i = i.strip()
+        if i not in parts:
+            parts.append(i)
+    print(parts)
+
+# select meal from list of meals
+def select_meal(meals):
+    orders = []
+    print("Todays meals:")
+    for i in meals:
+        print(str(i["order"]) + ": " + i["description"])
+        orders.append(str(i["order"]))
+    s = None
+    while s not in orders:
+        print(orders)
+        s = input("Select meal from above: ")
+    print(str(s) + " selected")
+    for i in meals:
+        if str(i["order"]) == s: return i
 
 #%%
-profile, ingredients = startup()
-
-#save_data(ingredients, "ingredients")
+# TODO:
+# rate selected meal
+# update taste profile
+    
+#%%
+main()
 
 #%%
-#meals = get_all_meals()
 
-todayMeals = get_today_meals()
 
-#%%
 
 
